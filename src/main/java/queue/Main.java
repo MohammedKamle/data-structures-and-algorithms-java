5b2195c439cbafc5932eec9b7acc6782d9986559
Mohammed Kamle <mohammed@Mohammeds-MacBook-Pro.local>
2021-09-30 13:02:38 +0530

    implemented the linear queue methods with array

---
+++ src/main/java/queue/Main.java
@@ -0,0 +1,38 @@
+package queue;
+
+public class Main {
+
+    public static void main(String[] args) {
+        QueueArray queue = new QueueArray(5);
+
+        // Enqueueing
+        queue.enQueue(1);
+        queue.enQueue(2);
+        queue.enQueue(3);
+        queue.enQueue(4);
+
+        System.out.println(queue.isFull());
+        queue.enQueue(5);
+        System.out.println(queue.isFull());
+
+        // Dequeuing
+        System.out.println(queue.deQueue());
+        System.out.println(queue.deQueue());
+        System.out.println(queue.deQueue());
+        System.out.println(queue.deQueue());
+
+        //Peeking
+        System.out.println("Peek value is "+queue.peek());
+
+        System.out.println(queue.isEmpty());
+        System.out.println(queue.deQueue());
+        System.out.println(queue.isEmpty());
+
+        queue.deleteQueue();
+
+
+
+
+
+    }
+}
---
+++ src/main/java/queue/QueueArray.java
@@ -0,0 +1,78 @@
+package queue;
+
+public class QueueArray {
+    int[] arr;
+    int topOfQueue;
+    int beginningOfQueue;
+
+    public QueueArray(int size) {
+        this.arr = new int[size];
+        this.topOfQueue = -1;
+        this.beginningOfQueue = -1;
+        System.out.println("The Queue is successfully created with size: " + size);
+    }
+
+    public boolean isFull() {
+        if (topOfQueue == arr.length - 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isEmpty() {
+        // when we dequeued all the elements from the queue beginningOfQueue will equal arr.length
+        if ((beginningOfQueue == -1) || (beginningOfQueue == arr.length)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void enQueue(int value) {
+        if (isFull()) {
+            System.out.println("The Queue is full!");
+        } else if (isEmpty()) {
+            beginningOfQueue = 0;
+            topOfQueue++;
+            arr[topOfQueue] = value;
+            System.out.println(value + " is successfully inserted in the queue");
+        } else {
+            topOfQueue++;
+            arr[topOfQueue] = value;
+            System.out.println(value + " is successfully inserted in the queue");
+        }
+    }
+
+    public int deQueue() {
+        if (isEmpty()) {
+            System.out.println("The Queue is empty!");
+            return -1;
+        } else {
+            int result = arr[beginningOfQueue];
+            beginningOfQueue++;
+            // if its the last element that is deQueued then beginningOfQueue > topOfQueue and
+            // so we will make necessary changes
+            if (beginningOfQueue > topOfQueue) {
+                beginningOfQueue = topOfQueue = -1; // as queue will be empty
+
+            }
+            return result;
+        }
+    }
+
+    public int peek(){
+        if (!isEmpty()){
+            return arr[beginningOfQueue];
+        }else {
+            System.out.println("The Queue is empty");
+            return -1;
+        }
+    }
+
+    // delete the queue
+    public  void deleteQueue(){
+        arr = null;
+        System.out.println("Queue is successfully deleted");
+    }
+}
