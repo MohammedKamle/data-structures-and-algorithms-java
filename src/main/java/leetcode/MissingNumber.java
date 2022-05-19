0620a8a15e68cea8f8993a007279a22edd3a1105
MohammedKamle <66168802+MohammedKamle@users.noreply.github.com>
2022-05-19 12:12:14 +0530

    Update MissingNumber.java

--- src/main/java/leetcode/MissingNumber.java
+++ src/main/java/leetcode/MissingNumber.java
@@ -1,7 +1,15 @@
 package leetcode;
 
 public class MissingNumber {
-    
+    public int missingNumber(int[] nums) {  
+        int n = nums.length;
+        int sum = n*(n+1)/2;
+        int actualSum=0;
+        for(int i=0; i<nums.length;i++){
+            actualSum += nums[i];
+        }
+        return sum-actualSum;
+    }
     public static void main(String[] args) {
         
     }
