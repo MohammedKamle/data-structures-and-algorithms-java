d1ece43bb1d0e8cf950e50d0648445a9f0ef27f0
mohammed-dunzo <104075408+mohammed-dunzo@users.noreply.github.com>
2022-06-13 12:48:46 +0530

    added leetcode 3Sum update

--- src/main/java/leetcode/ThreeSum.java
+++ src/main/java/leetcode/ThreeSum.java
@@ -5,6 +5,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class ThreeSum {
+    
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> result = new LinkedList<>();
