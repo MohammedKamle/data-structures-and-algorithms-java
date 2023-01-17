81c59902447abf782642d88e461f35e4ef356bbb
mohammed-dunzo <104075408+mohammed-dunzo@users.noreply.github.com>
2023-01-17 12:50:12 +0530

    added in topkfrquent java streams approach

--- src/main/java/leetcode/TopKFrequent.java
+++ src/main/java/leetcode/TopKFrequent.java
@@ -1,8 +1,7 @@
 package leetcode;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class TopKFrequent {
     public int[] topKFrequent(int[] nums, int k) {
@@ -21,24 +20,49 @@ public class TopKFrequent {
         //map.put(i, map.getOrDefault(i,0)+1);
 
         // storing k most frequent elements in result array
-        int index = 0 ;
+        int index = 0;
         int maxKey = 0;
-        while (index < k){
+        while (index < k) {
             int maxOccurence = Integer.MIN_VALUE;
-            for (int i : map.keySet()){
-                if (map.get(i)>maxOccurence){
+            for (int i : map.keySet()) {
+                if (map.get(i) > maxOccurence) {
                     maxOccurence = map.get(i);
                     maxKey = i;
                 }
             }
-            result[index++] =maxKey;
+            result[index++] = maxKey;
             // removing the max key from the map for next iteration for getting the next max
             map.remove(maxKey);
         }
         return result;
     }
 
+    // using java streams
+    public int[] topKFrequent_(int[] nums, int k) {
+        Map<Integer, Integer> elemCountMap = new HashMap<>();
+        for (int num : nums) {
+            elemCountMap.put(num, elemCountMap.getOrDefault(num, 0) + 1);
+        }
+        System.out.println(elemCountMap);
+        List<Integer> result = elemCountMap.entrySet().stream()
+                .sorted((a, b) -> b.getValue().compareTo(a.getValue()))
+                .limit(k)
+                .map(i -> i.getKey())
+                .collect(Collectors.toList());
+        int[] resultArr = new int[result.size()];
+        for(int i = 0; i < result.size(); i++) {
+            resultArr[i] = result.get(i);
+        }
+        return resultArr;
+    }
+
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(new TopKFrequent().topKFrequent(new int[]{1,1,1,2,2,3}, 2)));
+        System.out.println(Arrays.toString(new TopKFrequent().topKFrequent_(new int[]{1, 2, 2, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4}, 2)));
+        List<Integer> list = Arrays.asList(1,5,3,89,2,7,3);
+        // for descending  b.compareTo(a) and for ascending a.compareTo(b)
+        var res = list.stream()
+                .sorted((a,b)->b.compareTo(a))
+                .collect(Collectors.toList());
+        System.out.println(res);
     }
 }
