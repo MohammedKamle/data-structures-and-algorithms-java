950fb1855ab2a0cba86f730af7a503d411da237c
mohammed-dunzo <104075408+mohammed-dunzo@users.noreply.github.com>
2023-01-22 19:57:28 +0530

    longest conseq seq

--- src/main/java/leetcode/LongestConsecutiveSequence.java
+++ src/main/java/leetcode/LongestConsecutiveSequence.java
@@ -1,5 +1,32 @@
 package leetcode;
 
+import java.util.HashSet;
+import java.util.Set;
+
+// https://www.youtube.com/watch?v=P6RZZMu_maU
+
 public class LongestConsecutiveSequence {
 
+    public int longestConsecutive(int[] nums) {
+        if (nums.length == 0) return 0;
+        HashSet<Integer> set = new HashSet<>();
+        int ans = 1;
+        for (int num : nums)
+            set.add(num);
+        for (int num : nums) {
+            if (!set.contains(num - 1)) {
+                int count = 1;
+                while (set.contains(num + 1)) {
+                    num++;
+                    count++;
+                }
+                ans = Math.max(count, ans);
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new LongestConsecutiveSequence().longestConsecutive(new int[]{100,4,200,1,3,2}));
+    }
 }
