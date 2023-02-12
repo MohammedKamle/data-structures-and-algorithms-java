aa7a8b3be4a5cd620230a0a6852443e6203c4675
mohammed-dunzo <104075408+mohammed-dunzo@users.noreply.github.com>
2023-02-12 11:09:09 +0530

    added new approch to threeSum and added trapping rain water problem

--- src/main/java/leetcode/ThreeSum.java
+++ src/main/java/leetcode/ThreeSum.java
@@ -1,11 +1,12 @@
 package leetcode;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
 public class ThreeSum {
-    
+
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> result = new LinkedList<>();
@@ -17,19 +18,49 @@ public class ThreeSum {
                 while (leftPointer < rightPointer) {
                     if (nums[leftPointer] + nums[rightPointer] == sum) {
                         result.add(Arrays.asList(nums[i], nums[leftPointer], nums[rightPointer]));
-                        while (leftPointer < rightPointer && nums[leftPointer] == nums[leftPointer + 1]) leftPointer++;
-                        while (leftPointer < rightPointer && nums[rightPointer] == nums[rightPointer - 1])   rightPointer--;
+                        while (leftPointer < rightPointer && nums[leftPointer] == nums[leftPointer + 1])
+                            leftPointer++;
+                        while (leftPointer < rightPointer && nums[rightPointer] == nums[rightPointer - 1])
+                            rightPointer--;
                         leftPointer++;
                         rightPointer--;
-                    }else if (nums[leftPointer] + nums[rightPointer] > sum) {
+                    } else if (nums[leftPointer] + nums[rightPointer] > sum) {
                         rightPointer--;
-                    }else {
+                    } else {
                         leftPointer++;
                     }
                 }
             }
         }
         return result;
+    }
+
+    public List<List<Integer>> threeSum_(int[] nums) {
+        Arrays.sort(nums);
+
+        List<List<Integer>> res = new ArrayList<>();
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i == 0 || nums[i] != nums[i - 1]) {
+                int l = i + 1;
+                int r = nums.length - 1;
+                int threeSum = nums[i] + nums[l] + nums[r];
+                while (l < r) {
+                    if (threeSum == 0) {
+                        res.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                        while (l < r && (nums[l] == nums[l + 1])) l++;
+                        while (l < r && (nums[r] == nums[r - 1])) r--;
+                        l++;
+                        r--;
+                    } else if (threeSum > 0) {
+                        r--;
+                    } else {
+                        l++;
+                    }
+                }
+            }
+        }
+        return res;
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/leetcode/TrappingRainWater.java
@@ -0,0 +1,45 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class TrappingRainWater {
+
+    // Using array pre-processing, check the video on yt below
+    //https://www.youtube.com/watch?v=UZG3-vZlFM4
+    public int trap(int[] height) {
+        int[] leftMax = new int[height.length];
+        int[] rightMax = new int[height.length];
+
+        leftMax[0] = height[0];
+        rightMax[height.length - 1] = height[height.length - 1];
+
+        int max = height[0];
+        for (int i = 1; i < height.length; i++) {
+            if (height[i] > max) {
+                leftMax[i] = height[i];
+                max = height[i];
+            } else {
+                leftMax[i] = max;
+            }
+        }
+        max = height[height.length - 1];
+        for (int i = height.length - 2; i >= 0; i--) {
+            if (height[i] > max) {
+                rightMax[i] = height[i];
+                max = height[i];
+            } else {
+                rightMax[i] = max;
+            }
+        }
+        int waterTrapped = 0;
+        for (int i = 0; i < height.length; i++) {
+            waterTrapped += height[i] = Math.min(leftMax[i], rightMax[i]) - height[i];
+        }
+        return waterTrapped;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new TrappingRainWater().trap(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
+
+    }
+}
